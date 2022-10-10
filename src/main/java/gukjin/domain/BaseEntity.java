40ca2aaf0c9d680fa9ba459d332b1e164d9becda
=gukjin <internet2dot0@gmail.com>
2022-10-11 00:41:06 +0900

    Feat: @MappedSuperClass annotation

---
+++ src/main/java/gukjin/domain/BaseEntity.java
@@ -0,0 +1,26 @@
+package gukjin.domain;
+
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+public abstract class BaseEntity {
+    private String createdBy;
+    private LocalDateTime createdTime;
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public LocalDateTime getCreatedTime() {
+        return createdTime;
+    }
+
+    public void setCreatedTime(LocalDateTime createdTime) {
+        this.createdTime = createdTime;
+    }
+}
--- src/main/java/gukjin/domain/Order.java
+++ src/main/java/gukjin/domain/Order.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @Table(name = "ORDERS")
 @Entity
-public class Order {
+public class Order extends BaseEntity{
     @Id @GeneratedValue @Column(name = "ORDER_ID")
     private Long id;
 
